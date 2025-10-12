0d80760a89ca50c6ff58e3af8db681bcb9158d86
SHREYAS MADAKE <madakeshreyas@gmail.com>
2025-10-12 16:18:44 +0530

    Added BT Maximum Path Sum in BST Question

---
+++ DSAProjectFiles/src/BT/Q7_BT_MAX_PATHSUM.java
@@ -0,0 +1,21 @@
+package BT;
+
+public class Q7_BT_MAX_PATHSUM {
+    int ans=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return ans;
+    }
+    int helper(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        int left=helper(node.left);
+        int right= helper(node.right);
+        left=Math.max(0,left);
+        right=Math.max(0,right);
+        int pathsum = left +right +node.val;
+        ans=Math.max(ans,pathsum);
+        return Math.max(left,right) + node.val;
+    }
+}
