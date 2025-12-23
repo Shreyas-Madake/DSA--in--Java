67c5524944373717cbb1f91e5af6fc30d81780c9
SHREYAS MADAKE <madakeshreyas@gmail.com>
2025-12-23 19:40:03 +0530

    Added Daily Temperatures Question in NeetCode_150

---
+++ DSAProjectFiles/src/NeetCode_150/Q24_daily_temperatures.java
@@ -0,0 +1,19 @@
+package NeetCode_150;
+
+import java.util.Stack;
+
+public class Q24_daily_temperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int [] ans=new int[n];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                int index=stack.pop();
+                ans[index]=i-index;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
