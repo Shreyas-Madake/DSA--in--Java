584fe762eca4b5f7bbc910776204128a38866e99
SHREYAS MADAKE <madakeshreyas@gmail.com>
2025-12-25 21:35:04 +0530

    Added Binary Search Question in NeetCode_150

---
+++ DSAProjectFiles/src/NeetCode_150/Q25_binary_Search.java
@@ -0,0 +1,23 @@
+package NeetCode_150;
+
+public class Q25_binary_Search {
+    public int search(int[] nums, int target) {
+        int left=0;
+        int right= nums.length-1;
+        while(left<=right){
+            int mid= left + (right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                left=mid+1;
+
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return -1;
+
+    }
+}
