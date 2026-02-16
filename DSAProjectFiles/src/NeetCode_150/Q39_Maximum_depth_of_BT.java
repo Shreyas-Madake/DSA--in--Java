333d908a65544de75baa0986c55b3ddc2fb586de
SHREYAS MADAKE <madakeshreyas@gmail.com>
2026-02-16 14:01:18 +0530

    Added Maximum Depth Of BT Question in NeetCode_150

---
+++ DSAProjectFiles/src/NeetCode_150/Q39_Maximum_depth_of_BT.java
@@ -0,0 +1,28 @@
+package NeetCode_150;
+
+public class Q39_Maximum_depth_of_BT {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+
+}
