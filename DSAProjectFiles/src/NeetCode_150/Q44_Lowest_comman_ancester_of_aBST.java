2b3d3740296cde8d00b1a68901c679ae6ab7cd43
SHREYAS MADAKE <madakeshreyas@gmail.com>
2026-02-21 11:04:49 +0530

    Added Lowest Common Ancestor Of A BST Question in NeetCode_150

---
+++ DSAProjectFiles/src/NeetCode_150/Q44_Lowest_comman_ancester_of_aBST.java
@@ -0,0 +1,23 @@
+package NeetCode_150;
+
+public class Q44_Lowest_comman_ancester_of_aBST {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null){
+            return null;
+        }
+        if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left,p,q);         }
+
+
+        if(p.val>root.val && q.val>root.val){
+            return lowestCommonAncestor(root.right,p,q);         }
+        return root;
+
+    }
+}
