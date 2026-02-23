d2ec071cd06321f636f28683c6b90906d8aed01b
SHREYAS MADAKE <madakeshreyas@gmail.com>
2026-02-23 10:28:57 +0530

    Added Kth Largest Element In Stream Question in NeetCode_150

---
+++ DSAProjectFiles/src/NeetCode_150/Q47_Kth_largest_elemenyin_stream.java
@@ -0,0 +1,28 @@
+package NeetCode_150;
+
+import java.util.PriorityQueue;
+
+public class Q47_Kth_largest_elemenyin_stream {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+    public void KthLargest(int k, int[] nums) {
+        this.k=k;
+        this.minHeap=new PriorityQueue<>(k);
+        // to add initial elements to heap
+        for(int num:nums){
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if(minHeap.size()<k){
+            minHeap.add(val);
+        }else if(val>minHeap.peek()){
+            minHeap.poll();
+            minHeap.offer(val);
+        }
+        return minHeap.peek();
+
+    }
+}
+}
