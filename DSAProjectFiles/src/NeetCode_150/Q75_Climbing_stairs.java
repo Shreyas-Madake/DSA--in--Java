7b8ded5443049cb8f831e686697eba47ddb82d68
SHREYAS MADAKE <madakeshreyas@gmail.com>
2026-04-28 17:24:36 +0530

    Added Climbing Stairs Question in NeetCode_150

---
+++ DSAProjectFiles/src/NeetCode_150/Q75_Climbing_stairs.java
@@ -0,0 +1,17 @@
+package NeetCode_150;
+
+public class Q75_Climbing_stairs {
+    public int climbStairs(int n) {
+        //Base Case
+        if(n<=2){
+            return n;
+        }
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int state=3;state<=n;state++){
+            dp[state]=dp[state-1]+dp[state-2];
+        }
+        return dp[n];
+    }
+}
