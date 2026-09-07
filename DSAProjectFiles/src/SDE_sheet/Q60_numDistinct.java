059c86c63ee57fee113e29df43afb07d314609c4
SHREYAS MADAKE <madakeshreyas@gmail.com>
2026-09-07 16:29:11 +0530

    Added Distinct Subsequences Question in SDE_SHEET

---
+++ DSAProjectFiles/src/SDE_sheet/Q60_numDistinct.java
@@ -0,0 +1,35 @@
+package SDE_sheet;
+
+import java.util.Arrays;
+
+public class Q60_numDistinct {
+    public int numDistinct(String s, String t) {
+        int[][] dp=new int[s.length()][t.length()];
+
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        return solve(s,t,0,0,dp);
+
+    }
+    public int solve(String s, String t, int i, int j, int[][] dp){
+        if(j==t.length()){
+            return 1;
+        }
+        if(i==s.length()){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s.charAt(i)==t.charAt(j)){
+            int pick=solve(s,t,i+1,j+1,dp);
+            int nopick=solve(s,t,i+1,j,dp);
+
+            return dp[i][j]=pick+ nopick;
+
+        }
+        int nopick=solve(s,t,i+1,j,dp);
+        return dp[i][j]= nopick;
+    }
+}
